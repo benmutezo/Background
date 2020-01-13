9df6ffb2e0c540059982b66c4f303d9790b1e4a1
Barnard Mutezo <barnardmutezo@gmail.com>
2020-01-13 11:38:56 +0100

    updated files

---
+++ learning.js
@@ -0,0 +1,23 @@
+// var CSS = document.querySelector("h3");
+// var color1 = document.querySelector(".color1");
+// var color2 = document.querySelector(".color2");
+// var body = document.getElementById("background");
+
+// function color_picker() {
+//   body.style.background = `Linear-gradient(to right, ${color1.value}, ${color2.value})`;
+//   CSS.textContent = body.style.background + ";"
+// }
+// color1.addEventListener("input", color_picker);
+// color2.addEventListener("input", color_picker);
+
+
+
+function q1() {
+    var a = 5;
+    if(a > 1) {
+        a = 3;
+    }
+    alert(a);
+}
+
+
